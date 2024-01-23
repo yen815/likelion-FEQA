bad4362b30f34f0c91042b075cfbc75ca8f830ce
yen815 <chldpdms815@gmail.com>
2024-01-24 01:05:12 +0900

    실습

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Widget from '../components/Widget/Widget';
+import Button from '../components/Button/Button';
 
 // const styles = {
 //   app: {
@@ -12,14 +13,29 @@ function App() {
   return (
     <div className="app" /*  style={styles.app} */>
       <Widget
+        type="partly cloudy"
         temperature={-0.9}
         summary="어제보다 1.3° 낮아요"
         location="서울"
       />
+
+      <Button loading={!false}>저장</Button>
+
       <Widget
+        type="rainy"
         temperature={-0.7}
         summary="어제보다 0.1° 높아요"
         location="도쿄"
+      />
+
+      <Button disable>취소</Button>
+      {/* disable을 없애면 버튼 클릭 가능, 있으면 클릭 불가 */}
+
+      <Widget
+        type="sunny"
+        temperature={-1.2}
+        summary="어제보다 1.1° 낮아요"
+        location="베이징"
       />
     </div>
   );

c8c91958f13f47f9880b0b1f7eb325dc990725e6
yen815 <chldpdms815@gmail.com>
2024-01-23 14:14:44 +0900

    [실습] 컴포넌트 속성을 사용한 효율적 재사용

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,17 +1,17 @@
 import './Widget.css';
 
-function Widget() {
+function Widget(props) {
   return (
     <article className="widget widget__weather">
-      <h2 className="sr-only">날씨</h2>
+      <h2 className="sr-only">{props.location} 날씨</h2>
       <img
         src="/images/weather-partly-cloudy.svg"
         alt="맑고 구름 조금"
         title="맑고 구름 조금"
       />
       <div className="weather__info" role="group">
-        <span className="weather__info--template">-0.9°</span>
-        <span className="weather__info--description">어제보다 1.3° 낮아요</span>
+        <span className="weather__info--template">{props.temperature}</span>
+        <span className="weather__info--description">{props.summary}</span>
       </div>
     </article>
   );

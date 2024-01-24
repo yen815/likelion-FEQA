efd4cac446d92f0f15ac53c55a57cb2699a15a2e
yen815 <chldpdms815@gmail.com>
2024-01-24 09:58:21 +0900

    파일구성

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost', // default: 'localhost'
+    port: 3000, // default: 5173
+    open: true, // default: false
+  },
 });

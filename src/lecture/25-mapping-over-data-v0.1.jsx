4123d6a984bcf7d680134f38935b0e693b3ce679
yen815 <chldpdms815@gmail.com>
2024-01-25 03:02:31 +0900

    실습 - 데이터 매핑

---
+++ src/lecture/25-mapping-over-data-v0.1.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/jsx-key */
+
+import './25-mapping-over-data.css';
+import contactData from '../data/contacts.json';
+
+export default function Exercise() {
+  const { items } = contactData;
+
+  const children = items.map(({ id, name, email }) => {
+    return (
+      <li style={listStyle}>
+        <span>{id}</span>
+        <span>{name}</span>
+        <span>{email}</span>
+      </li>
+    );
+  });
+  return <ul>{children}</ul>;
+
+  // x = 3
+  // y = x + 7
+
+  // return (
+  //   <ul>
+  //     {items.map(({ id, name, email }) => (
+  //       <li style={listStyle}>
+  //         <span>{id}</span>
+  //         <span>{name}</span>
+  //         <span>{email}</span>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
+}
+
+const listStyle = {
+  display: 'flex',
+  flexFlow: 'column',
+  marginBlockEnd: 20,
+};

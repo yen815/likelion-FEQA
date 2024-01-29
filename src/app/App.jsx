9ec51e8b4071af7ebbaafe0978841d0b0e932f15
yen815 <chldpdms815@gmail.com>
2024-01-29 10:19:30 +0900

    [정리] - className 문자 병합 - style 객체 합성 - props 객체 합성

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,9 @@
+import Exercise from '../lecture/29-a11y-hidden-component-v3';
 import './App.css';
-import Exercise from '../lecture/29-a11y-hidden-component-v2';
-import { getStaticImage } from '../utils/getStaticAsset';
-import { Avatar } from '../components';
 
 function App() {
   return (
     <div className="app">
-      <Avatar photo={getStaticImage('faces/man-01.jpg')} />
       <Exercise />
     </div>
   );
